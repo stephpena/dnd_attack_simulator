ef43e3386d2f9f3486bf1c7cde48b8a4ff39095d
Stephanie Peña <stephaniebpena@gmail.com>
2018-05-28 15:29:48 -0700

    created inital character and spell atk class files

---
+++ src/character.py
@@ -0,0 +1,18 @@
+class Character(object):
+    ''' This class generates character info to be used by attack classes
+
+    Methods
+    --------
+
+
+    Attributes
+    -----------
+
+    '''
+
+    def __init__(self):
+        '''
+        Parameters
+        -----------
+
+        '''
---
+++ src/spell.py
@@ -0,0 +1,18 @@
+class Spell(object):
+    ''' This class generates average attack damage sums based on given inputs for spell attacks
+
+    Methods
+    --------
+
+
+    Attributes
+    -----------
+
+    '''
+
+    def __init__(self):
+        '''
+        Parameters
+        -----------
+
+        '''
